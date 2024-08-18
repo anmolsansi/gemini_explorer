6bd20f10478e42a476ffc7f0722e777002b1428c
Anmol Sansi <asasni@hawk.iit.edu>
2024-08-18 17:50:15 -0500

    Code updated

--- mission-gemini_flights_backend/gemini_explorer.py
+++ mission-gemini_flights_backend/gemini_explorer.py
@@ -18,9 +18,64 @@ model = GenerativeModel(
 )
 
 chat = model.start_chat()
-prompt = "generate a description of a peaceful landscape in 20 words"
-response = model.generate_content(prompt)
 
-# response = chat.ask(prompt)
-st.write(response)
-st.code("streamlit run gemini_explorer.py")
+
+# helper function to display and send streamlit messages
+def llm_function(chat: ChatSession, query):
+    response = chat.send_message(query)
+    output = response.candidates[0].content.parts[0].text
+    
+    st.write("Model:", output)
+
+    st.session_state.messages.append(
+    {
+    "role": "user",
+    "content": query
+    })
+
+    st.session_state.messages.append(
+    {
+    "role": "model",
+    "content": output})
+
+    return response
+
+
+st.title("Gemini Explorer")
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = [ ]
+
+for message in st.session_state.messages:
+    content = Content (
+        role = message["role"],
+        parts = [Part.from_text(message["content"])])
+
+    try:
+        if message["role"] == "user":
+            st.write("User:", message["content"])
+        elif message["role"] == "model":
+            st.write("Model:", message["content"])
+
+    except Exception as e:
+        print(e)
+        print(message)
+
+    chat.history.append(content)
+
+if len(st.session_state.messages) == 0:
+    # initial_prompt = "Introduce yourself as ReX, an assistant powered by Google Gemini. You use emojis to be interactive"
+    name = st.text_input("Enter your name:")
+    print(name)
+
+    if name:
+        initial_prompt = "Arrr matey! Introduce yourself as ReX, a savvy assistant powered by Google Gemini. Ye use emojis to be interactive. Also use this name to address user - "+ name
+        llm_function(chat, initial_prompt)
+
+query = st.text_input("Gemini Explorer")
+
+if query:
+    st.write("User:", query)
+
+    response = llm_function(chat, query)
